b05412223a68fab298752f8be04263f38969e5a1
km_99 <56396995+KasiaMajchrzak@users.noreply.github.com>
2020-10-18 12:29:08 +0200

    Revert "Added registration functionality along with tests. (#10)" (#11)

--- src/main/java/pl/sda/auctions/services/UserService.java
+++ src/main/java/pl/sda/auctions/services/UserService.java
@@ -1,15 +1,11 @@
 package pl.sda.auctions.services;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.sda.auctions.model.Role;
 import pl.sda.auctions.model.User;
 import pl.sda.auctions.repository.UserRepository;
 
-import java.util.Optional;
-
-@Slf4j
 @Service
 public class UserService {
 
@@ -24,22 +20,11 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public Optional<User> findByEmail(String email){
-        return userRepository.findByEmail(email);
-    }
-
-    public boolean checkIfUserExistsEmail(String email) {
-        boolean existsEmail = userRepository.findByEmail(email).isPresent();
-        return existsEmail;
-    }
-
-    public boolean checkIfUserExistsName(String name) {
-        boolean existsName = userRepository.findByName(name).isPresent();
-        return existsName;
+    public boolean checkIfUserExists(String email) {
+        return userRepository.findByEmail(email).isPresent();
     }
 
     public void createUser(String name, String email, String password, Role role) {
-        log.info("entering createUser...{}", email);
         var user = new User(
                 null,
                 email,
@@ -49,7 +34,6 @@ public class UserService {
                 role
 
         );
-        log.info("user {}", user);
         userRepository.save(user);
     }
 }
